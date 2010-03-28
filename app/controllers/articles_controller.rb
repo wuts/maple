6913f8a253a96ee9b382be9ebd20c52e19874ea9
wuts <wuts@wuts-desktop.(none)>
2010-03-28 11:27:06 +0800

    modified articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,19 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.xml
   def show
+
     @article = Article.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @article }
     end
+  end
+  
+  def inline_render
+    @str="render"
+    render :inline=>
+      "<%=@str%>"
   end
 
   # GET /articles/new
