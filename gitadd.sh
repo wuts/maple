23806a9e094853dd4494d94730117b43d82293c5
wuts <wuts@wuts-desktop.(none)>
2010-04-27 00:00:54 +0800

    update gitadd.sh

--- gitadd.sh
+++ gitadd.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-git add    app/controllers/admin/admin_controller.rb
-git add   app/controllers/admin/articles_controller.rb
-git add   app/controllers/articles_controller.rb
-git add   app/controllers/users_controller.rb
-git add   app/views/articles/show.html.erb
-git add   app/views/articles/show_by_slug.html.erb
-git add   app/views/home/index.html.erb
-git add   app/views/layouts/admin.html.erb
-git add   app/views/shared/_admin_menu_en.html.erb
-git add   config/locales/en.yml
-git add   config/locales/zh_CN.yml
-git add   gitadd.sh
-git add   public/stylesheets/admin/admin.css
-git add   public/stylesheets/boxes.css
 
-git add	app/views/layouts/login.html.erb
 
+git add	config/tiny_mce.yml
+git add	config/yui_editor.yml
+git add	public/javascripts/tiny_mce/
+git add	public/system/images/18/large/rails.png
+git add	public/system/images/18/medium/
+git add	public/system/images/18/original/rails.png
+git add	public/system/images/18/thumb/rails.png
+git add	public/system/images/19/
