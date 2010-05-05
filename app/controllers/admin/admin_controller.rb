d2c48de7a61cde36a6206ff14ba1b0f3565c629a
wuts <wuts@wuts-desktop.(none)>
2010-05-05 22:52:13 +0800

    modified:   ../app/controllers/admin/admin_controller.rb 	modified:   ../app/controllers/admin/articles_controller.rb 	modified:   ../app/controllers/articles_controller.rb 	modified:   ../config/environment.rb 	schema.rb.old 	../public/system/images/20/

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,8 @@
 class Admin::AdminController < ApplicationController
   layout "admin"
-  before_filter :login_required
+  before_filter :http_basic_authenticate_required,:login_required
   include AuthenticatedSystem  
+  
  private
  def authenticate
     authenticate_or_request_with_http_basic do |name,password|
@@ -12,6 +13,12 @@ class Admin::AdminController < ApplicationController
  def login_required
    if !logged_in?
       redirect_to '/login'
+   end
+ end
+ 
+ def http_basic_authenticate_required
+   authenticate_or_request_with_http_basic do |username,password|
+     username=="admin" && password=="8888"
    end
  end
 end
